ab11e3c1d310ab264b557bbb081a8e8eb929ae4e
Zeeshan Akhlaq <amerschzeesakh@gmail.com>
2021-09-24 20:24:15 +0100

    Initial Commit, adding SQLite database for integration with carsearch activity to allow vehicle search

---
+++ app/src/main/java/com/example/projectsupercars/NumberPlateCheck.java
@@ -0,0 +1,14 @@
+package com.example.projectsupercars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NumberPlateCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_plate_check);
+    }
+}
