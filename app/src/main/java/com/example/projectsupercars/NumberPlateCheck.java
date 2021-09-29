e75fde34b42022eca9d7cb0cccdf436a5fbff5c7
Zeeshan Akhlaq <amerschzeesakh@gmail.com>
2021-09-29 22:08:09 +0100

    Added WebView for Number Plate Checking option, allows direct access to Gov.uk website for easy Tax and MOT information.

--- app/src/main/java/com/example/projectsupercars/NumberPlateCheck.java
+++ app/src/main/java/com/example/projectsupercars/NumberPlateCheck.java
@@ -3,12 +3,33 @@ package com.example.projectsupercars;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class NumberPlateCheck extends AppCompatActivity {
+
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_plate_check);
+
+        web = findViewById(R.id.webView);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new Callback());
+        web.loadUrl("https://vehicleenquiry.service.gov.uk/");
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+
     }
 }
