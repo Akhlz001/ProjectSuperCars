3885315d991ae33554ff4a2b485512b0723a833d
Zeeshan Akhlaq <amerschzeesakh@gmail.com>
2021-10-03 17:03:50 +0100

    SQLite database integration complete, application is working allowing vehicle input and deletion.

--- app/src/main/java/com/example/projectsupercars/CarSearch.java
+++ app/src/main/java/com/example/projectsupercars/CarSearch.java
@@ -1,19 +1,99 @@
 package com.example.projectsupercars;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class CarSearch extends AppCompatActivity {
 
     EditText name, plate, mileage;
     Button insert, delete, view;
+    DBHelper DB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_search);
+
+        name = findViewById(R.id.car);
+        plate = findViewById(R.id.numberplate);
+        mileage = findViewById(R.id.mileage);
+
+        insert = findViewById(R.id.insertData);
+        delete = findViewById(R.id.deleteData);
+        view = findViewById(R.id.viewData);
+
+        DB = new DBHelper(this);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String vehiclenameTXT = name.getText().toString();
+                String numberplateTXT = plate.getText().toString();
+                String mileageTXT = mileage.getText().toString();
+
+                Boolean checkinsertdata = DB.insertvehicledata(vehiclenameTXT, numberplateTXT, mileageTXT);
+                if(checkinsertdata==true)
+                {
+                    Toast.makeText(CarSearch.this, "New Vehicle Added", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(CarSearch.this, "Cannot add the same vehicle details twice!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String vehiclenameTXT = name.getText().toString();
+                Boolean checkdeletedata = DB.deletevehicledata(vehiclenameTXT);
+                if(checkdeletedata==true)
+                {
+                    Toast.makeText(CarSearch.this, "Vehicle deleted from database", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(CarSearch.this, "Cannot delete the same vehicle details twice!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getvehicledata();
+                if(res.getCount()==0)
+                {
+                    Toast.makeText(CarSearch.this, "Vehicle does not exist in the database", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext())
+                {
+                    buffer.append("vehicleName :"+res.getString(0) +"\n");
+                    buffer.append("numberPlate :"+res.getString(1) +"\n");
+                    buffer.append("mileage :"+res.getString(2) +"\n\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(CarSearch.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectsupercars/DBHelper.java
+++ app/src/main/java/com/example/projectsupercars/DBHelper.java
@@ -1,4 +1,63 @@
 package com.example.projectsupercars;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "Userdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Userdetails(vehicleName TEXT primary key, numberPlate TEXT, mileage TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists Userdetails");
+    }
+
+    public Boolean insertvehicledata(String vehicleName, String numberPlate, String mileage) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("vehicleName", vehicleName);
+        contentValues.put("numberPlate", numberPlate);
+        contentValues.put("mileage", mileage);
+        long result = DB.insert("Userdetails", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Boolean deletevehicledata(String vehicleName) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where vehicleName = ?", new String[]{vehicleName});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Userdetails", "vehicleName=?", new String[]{vehicleName});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public Cursor getvehicledata () {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails", null);
+        return cursor;
+
+    }
 }
