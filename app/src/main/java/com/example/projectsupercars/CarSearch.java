d3e474b22b78b0f3c9374bf6802f3eb5144fd78e
Zeeshan Akhlaq <amerschzeesakh@gmail.com>
2021-10-17 17:34:56 +0100

    Added different website for tax checking, using third party dependency to allow vehicle checks

--- app/src/main/java/com/example/projectsupercars/CarSearch.java
+++ app/src/main/java/com/example/projectsupercars/CarSearch.java
@@ -1,5 +1,6 @@
 package com.example.projectsupercars;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,16 +10,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class CarSearch extends AppCompatActivity {
 
     EditText name, plate, mileage;
+    TextView content;
     Button insert, delete, view;
     DBHelper DB;
 
@@ -30,6 +41,7 @@ public class CarSearch extends AppCompatActivity {
         name = findViewById(R.id.car);
         plate = findViewById(R.id.numberplate);
         mileage = findViewById(R.id.mileage);
+        content = findViewById(R.id.content);
 
         insert = findViewById(R.id.insertData);
         delete = findViewById(R.id.deleteData);
@@ -56,22 +68,38 @@ public class CarSearch extends AppCompatActivity {
                     Toast.makeText(CarSearch.this, "Cannot add the same vehicle details twice!", Toast.LENGTH_SHORT).show();
                 }
 
-                try {
+                OkHttpClient client = new OkHttpClient();
 
-                    String addr = "https://cartaxcheck.co.uk/free-car-check/?vrm=LN11VUM";
-                    URL url = new URL(addr);
-                    HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                    httpCon.setUseCaches(false);
-                    httpCon.setAllowUserInteraction(false);
+                String url = "https://www.carveto.co.uk/road-tax-status/?affiliate=car+tax+check+main+content&carVrm="+plate.getText().toString();
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            String motCheck = response.body().string();
+
+                            CarSearch.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    content.setText(motCheck);
+                                }
+                            });
+                        }
+                    }
+                });
 
             }
         });
+
 
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,3 +145,4 @@ public class CarSearch extends AppCompatActivity {
         });
     }
 }
+
